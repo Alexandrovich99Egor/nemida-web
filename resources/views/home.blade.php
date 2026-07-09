bf8746d278be4660eb11c0d6477e7553da7a083a
yehor <managerrifbar@gmail.com>
2026-07-09 18:05:49 +0300

    remove Dependabot configuration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+    1111111111111111
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
 
-require __DIR__.'/settings.php';
+Route::get('home', [HomeController::class, 'show'])->name('home');
